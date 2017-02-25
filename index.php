5c43b6dc11763da0a720f0565b05a909e0e24169
Andrey <drn.exp@gmail.com>
2017-02-25 04:29:14 +0200

    Create Project
    Migrate from .Net

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+$loader = require_once 'vendor/autoload.php';
+
+use src\Application;
+
+$app = new Application();
+$app->say();
---
+++ src/Application.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace src;
+
+class Application
+{
+    public function say()
+    {
+        echo 'Hello World' . PHP_EOL;
+    }
+}
