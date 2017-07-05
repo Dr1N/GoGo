245879ad24d7685eca1c0374c0f64ce3eaa7cea3
Andrey <drn.exp@gmail.com>
2017-07-05 05:50:01 +0300

    Remove test methods from utils

--- src/Utils.php
+++ src/Utils.php
@@ -2,13 +2,8 @@
 
 namespace src;
 
-use GuzzleHttp\Client;
 use src\models\Ad;
 use src\models\Image;
-use GuzzleHttp\Psr7\Request;
-use GuzzleHttp\Pool;
-use GuzzleHttp\Psr7\Response;
-use DiDom\Document;
 
 class Utils
 {
@@ -46,39 +41,6 @@ class Utils
 
     static public function run()
     {
-        $url = 'http://bucha.ukrgo.com/';
-        $adsCount = Parser::getAdsCount($url);
-        echo 'ADS: ' . $adsCount . PHP_EOL;
-        $pages = (int)ceil($adsCount / Parser::ADS_PER_PAGE);
-        echo 'FIND: ' . $pages . ' Pages' . PHP_EOL;
-
-        
-        $requests = function ($total) use ($url) {
-            for ($i = 1; $i <= $total; $i++) {
-                $currentUrl = $url . Parser::CATEGORY;
-                yield new Request('GET', ($i > 1 ? $currentUrl . '?' . Parser::SEARCH_PARAMS . $i : $currentUrl));
-            }
-        };
-        $client = new Client();
-        $pool = new Pool($client, $requests($pages), [
-            'concurrency' => 5,
-            'fulfilled' => function (Response $response, $index)  {
-                $document = new Document($response->getBody()->getContents());
-                list($urls, $isContinue) = Parser::getUrlsFromPage($document, null);
-                $result = array_merge(self::$res, $urls);
-                self::$res = array_merge(self::$res, $urls);
-                echo count($result) . PHP_EOL;
-            },
-            'rejected' => function ($reason, $index) {
-                echo $index . ' Fail!'  . $reason . PHP_EOL;
-            },
-        ]);
-        $promise = $pool->promise();
-        $promise->wait();
-
-        echo 'DONE!' . PHP_EOL;
-        echo count(array_unique(self::$res)) . PHP_EOL;
-
-        return self::$res;
+        echo 'Run from Utils' . PHP_EOL;
     }
 }
