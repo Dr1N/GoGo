940a6b2577a375753a524f04b5a49ce91c1b057f
Andrey <drn.exp@gmail.com>
2017-09-25 22:59:37 +0300

    test

--- app.php
+++ app.php
@@ -8,7 +8,7 @@ require_once 'vendor/autoload.php';
 use src\Application;
 
 $app = new Application();
-
+Application::log('Test Cron', 'app', true);
 file_put_contents('test.txt', 'Hello from Cron' . PHP_EOL, FILE_APPEND);
 for ($i = 0; $i < $argc; $i++) {
     file_put_contents('test.txt', $argv[$i] . PHP_EOL, FILE_APPEND);
